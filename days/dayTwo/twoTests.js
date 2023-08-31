a266bda0984b3521f58301caa2998319527faa9e
Maya <mayabaror@gmail.com>
2023-08-31 15:32:03 +0300

    ss

--- days/dayTwo/twoTests.js
+++ days/dayTwo/twoTests.js
@@ -69,72 +69,53 @@ async function start2tests() {
                                         clearInterval(sessionInterval2Test2);
                                         reset_blueCar();
                                         reset_redCar();
-                                        document.getElementById("redButton").style.display = "none";
-                                        document.getElementById("blueButton").style.display = "none";
-                                        setTimeout(() => {
-                                            document.getElementById("startYellowTestButton").style.display = "inline";
-                                        }, 1000)
-                                        setTimeout(() => {
-                                            //document.getElementById('iframe-element').classList.remove('hidden');
-                                            document.getElementById("iframe-element").src = "../../timer/timer.html";
-                                            document.getElementById("iframe-element").style.display = "inline";
-                                            document.getElementById("iframe-element").style.top = "18%";
-                                        }, 4000)
-                                        setTimeout(() => {
+                                        let startButtons = async function () {
                                             clearInterval(sessionInterval2Test2);
-                                            document.getElementById("redButton").style.display = "inline";
-                                            document.getElementById("blueButton").style.display = "inline";
-                                            document.getElementById("startYellowTestButton").style.display = "none";
-                                            document.getElementById("iframe-element").src = "";
-                                            document.getElementById("iframe-element").style.display = "none";
-                                            let startButtons = async function () {
-                                                showButtons();
-                                                let endButtonsTest = await startIntervalButtons();
-                                                if (endButtonsTest == "done4") {
-                                                    clearInterval(sessionIntervalButtons);
-                                                    reset_blueCar();
-                                                    reset_redCar();
-                                                    // reset_yellowCar();
-                                                    document.getElementById("blueButton").style.display = "none";
-                                                    document.getElementById("redButton").style.display = "none";
-                                                    // setTimeout(() => {
-                                                    //     document.getElementById("endYellowTestButton").style.display = "inline";
-                                                    //     document.getElementById("iframe-element2").style.top = "5%"
-                                                    //     document.getElementById("iframe-element2").style.display = "inline";
-                                                    //     document.getElementById("iframe-element2").src = "../../timer/timer2.html";
-                                                    // }, 1000)
-                                                    // setTimeout(() => {
-                                                    //     document.getElementById("endYellowTestButton").style.display = "none";
-                                                    //     document.getElementById("iframe-element2").style.display = "none";
-                                                    //     document.getElementById("iframe-element2").src = "";
-                                                    //     document.getElementById("redButton").style.display = "inline";
-                                                    //     document.getElementById("blueButton").style.display = "inline";
-                                                    //     intervalDone.push("done5");
-                                                    //     let endYellow = async function () {
-                                                    //         let doneTwoTests = await startInterval2Tests3();
-                                                    //         if (doneTwoTests == "done5") {
-                                                    //             // reset_blueCar();
-                                                    //             // reset_redCar();
-                                                    resolve("done");
-                                                    //         }
-                                                    //     }
-                                                    //     endYellow();
-                                                    // }, 7000)
-                                                }
+                                            showButtons();
+                                            let endButtonsTest = await startIntervalButtons();
+                                            if (endButtonsTest == "done4") {
+                                                clearInterval(sessionIntervalButtons);
+                                                reset_blueCar();
+                                                reset_redCar();
+                                                // reset_yellowCar();
+                                                document.getElementById("blueButton").style.display = "none";
+                                                document.getElementById("redButton").style.display = "none";
+                                                // setTimeout(() => {
+                                                //     document.getElementById("endYellowTestButton").style.display = "inline";
+                                                //     document.getElementById("iframe-element2").style.top = "5%"
+                                                //     document.getElementById("iframe-element2").style.display = "inline";
+                                                //     document.getElementById("iframe-element2").src = "../../timer/timer2.html";
+                                                // }, 1000)
+                                                // setTimeout(() => {
+                                                //     document.getElementById("endYellowTestButton").style.display = "none";
+                                                //     document.getElementById("iframe-element2").style.display = "none";
+                                                //     document.getElementById("iframe-element2").src = "";
+                                                //     document.getElementById("redButton").style.display = "inline";
+                                                //     document.getElementById("blueButton").style.display = "inline";
+                                                //     intervalDone.push("done5");
+                                                //     let endYellow = async function () {
+                                                //         let doneTwoTests = await startInterval2Tests3();
+                                                //         if (doneTwoTests == "done5") {
+                                                //             // reset_blueCar();
+                                                //             // reset_redCar();
+                                                resolve("done");
+                                                //         }
+                                                //     }
+                                                //     endYellow();
+                                                // }, 7000)
+
                                             }
-                                            startButtons()
-                                        }, 20000)
+                                        }
+                                        startButtons()
                                     }
-
                                 }
-
                                 afterStarTest();
-                            }, 7000)
+                            }, 4000)
                         }
+
                     }
                     startStarTest();
                 }, 17000)
-
             }
         }
         startIntervalTest();
