b4f335eec1d02095c85e3de32fe26746d0da5e59
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-09-04 14:41:07 +0300

    ss

--- days/dayThree/indexThree.js
+++ days/dayThree/indexThree.js
@@ -47,7 +47,7 @@ function timeline() {
                                     if (startTrainning == "done3") {
                                         setTimeout(() => {
                                             document.getElementById("startDevtestButtonRed").style.display = "inline";
-                                            document.getElementById("iframe-element").style.top = "1%"
+                                            document.getElementById("iframe-element").style.top = "5%"
                                             document.getElementById("iframe-element").src = "../../timer/timer.html";
                                             document.getElementById('iframe-element').classList.remove('hidden');
                                             document.getElementById("iframe-element").style.display = "inline";
