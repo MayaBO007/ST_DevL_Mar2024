b7b141795499434e4775560db327541d8c2c45b8
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2024-02-29 16:10:26 +0200

    sss

--- dayOrganizer.js
+++ dayOrganizer.js
@@ -3,20 +3,21 @@ function moveToDay() {
     platform.getAllSessions().then((data) => {
         getIndexSessionData(data).then((indexI) => {
             studySessionData = data[indexI];
-            if (extractDeviceName(userAgent) == studySessionData.userDevice) {
-                if ((typeof studySessionData == "undefined") || (studySessionData.doneInstructions == "")) {
-                    platform.goToUrl("instructions/instructions.html");
-                } else if (studySessionData.doneDay1 != "doneDayOne") {
-                    if (studySessionData.doneDay1 == "") {
-                        platform.goToUrl("days/dayOne/training.html");
+
+            if ((typeof studySessionData == "undefined") || (studySessionData.doneInstructions == "")) {
+                platform.goToUrl("instructions/instructions.html");
+            } else if (studySessionData.doneDay1 != "doneDayOne") {
+                if (studySessionData.doneDay1 == "") {
+                    platform.goToUrl("days/dayOne/training.html");
+                } else {
+                    if (window.matchMedia("(orientation: landscape)").matches) {
+                        document.getElementById("problem").style.display = "inline";
                     } else {
-                        if (window.matchMedia("(orientation: landscape)").matches) {
-                            document.getElementById("problem").style.display = "inline";
-                        } else {
-                            document.getElementById("problem_hor").style.display = "inline";
-                        }
+                        document.getElementById("problem_hor").style.display = "inline";
                     }
-                } else if ((studySessionData.doneDay1 == "doneDayOne") && (studySessionData.doneDay2 != "doneDayTwo")) {
+                }
+            } else if (extractDeviceName(userAgent) == studySessionData.userDevice) {
+                if ((studySessionData.doneDay1 == "doneDayOne") && (studySessionData.doneDay2 != "doneDayTwo")) {
                     if ((studySessionData.doneDay2 == "") || (studySessionData.subId == "64eda725d2c8fb647d0adc6e")) {
                         platform.goToUrl("days/dayTwo/dayTwo.html");
                     } else {
