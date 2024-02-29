20c65db0f7977813b2e6f931eb431f81f8d013b8
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2024-02-29 11:11:07 +0200

    sss

--- days/dayOne/trainingDay.js
+++ days/dayOne/trainingDay.js
@@ -24,16 +24,14 @@ let blue_yellow = false;
 
 redElement.addEventListener("click", function () {
     allRedPresses.push(new Date().getTime() - milliseconds);
-    this.classList.add('clicked');
     setTimeout(() => {
-        this.classList.remove('clicked');
+        redElement.style.transform = "translateY(0px)";
     }, 100); // Adjust the delay as nee
 });
 blueElement.addEventListener("touchstart", function () {
     allBluePresses.push(new Date().getTime() - milliseconds);
-    this.classList.add('clicked');
     setTimeout(() => {
-        this.classList.remove('clicked');
+        blueElement.style.transform = "translateY(0px)";
     }, 100); // Adjust the delay as nee
 });
 redElement.addEventListener("contextmenu", function (event) {
